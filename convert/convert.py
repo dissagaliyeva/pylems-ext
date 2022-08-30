736b113aadc2d9960b733848128541baad520bb6
Dinara Issagaliyeva <dinarissaa@gmail.com>
2022-08-30 09:57:41 +0400

    Add a function to extract relevant lines of code

--- convert/convert.py
+++ convert/convert.py
@@ -7,12 +7,7 @@ content = []
 # instantiate the model
 model = lems.Model()
 
-with open(FILE) as f:
-    for line in f.readlines():
-        if not line.startswith('#') and not line.startswith('import') and not line.startswith('from') and line != '\n':
-            content.append(line.strip('\n').strip())
 
-print(content)
 
 for c in content:
     if 'rose' in c.lower():
--- convert/utils.py
+++ convert/utils.py
@@ -4,8 +4,35 @@
 #               'LARTER_BREAKSPEAR', 'LINEAR', 'MONTBRIO_PAZO_ROXIN', 'REDUCED_SET_FITZ_HUGH_NAGUMO',
 #               'REDUCED_SET_HINDMARSH_ROSE', 'REDUCED_WONG_WANG', 'REDUCED_WONG_WANG_EXCH_INH', 'SUP_HOPF ',
 #               'WILSON_COWAN', 'ZERLAUT_FIRST_ORDER', 'ZERLAUT_SECOND_ORDER', 'ZETTERBERG_JANSEN']
-
+import os.path
 
 TVB_MODELS = {
     'ReducedSetHindmarshRose': 'hindmarshRose'
 }
+
+
+# open file and prepare the dataset
+def open_file(path: str) -> list:
+    """
+
+    :param path:
+    :return:
+    """
+
+    # verify the path exists
+    assert os.path.exists(path), f'File at location {path} does not exist'
+
+    # define an array to store line-by-line iteration
+    contents = []
+    
+    # open file
+    with open(path) as file:
+        # iterate over lines of code and pick the relevant lines only
+        for line in file.readlines():
+            # ignore comments, empty lines, and imports
+            if not line.startswith('#') and not line.startswith('import') and \
+               not line.startswith('from') and line != '\n':
+                # save relevant lines
+                contents.append(line.strip('\n').strip())
+
+    return contents
