352e5082714ab31b429a09a984fdf277f305a4c3
Dinara Issagaliyeva <dinarissaa@gmail.com>
2022-09-05 13:06:34 +0400

    Add more tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -14,13 +14,19 @@ def test_models():
     # test default models with no parameters
     model = Models()
     path = os.path.join('examples', 'default_param.xml')
+    check(model, path)
 
+    # check with uid and suffix
+    model = Models(uid='delta_times', suffix='50healthy')
+    path = os.path.join('examples', 'desc-50healthy_param.xml')
+    check(model, path)
+
+
+def check(model, path):
     assert model.params == {}, 'No parameters expected'
     assert isinstance(model.model, lems.Model), 'Model must be of lems.api.Model instance'
     assert os.path.exists(path), f'Expected {path} to be present'
     assert model.comp_type in ['SJHM3D', 'WongWang']
-
-    # open param.xml
 
 
 if __name__ == '__main__':
